4f95bddb4373b1d7762e79daf110b9e99524a9d6
MaksimHank <orthopedicsalon5@gmail.com>
2022-05-05 21:54:30 +0300

    Made Fibbonachi

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -13,3 +13,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/fibbonachi/{n}")
+def fibbonachi(n: int):
+    #  """Рекурсия"""
+    if n in {0, 1}:
+        return n
+    result = fibbonachi(n - 1) + fibbonachi(n - 2)
+    return result
+
+
