8dd3fce7a6d404d96a12ed59b2b8a08f3c88d464
MaksimHank <orthopedicsalon5@gmail.com>
2022-05-08 09:57:38 +0300

    Made Fibbonachi with get request

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/fibbonachi/{n}")
+@app.post("/fibbonachi/{n}")
 def fibbonachi(n: int):
     #  """Рекурсия"""
     if n in {0, 1}:
