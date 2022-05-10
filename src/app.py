4b09fcbb545cfb403c8e5235f33abb4418753320
MaksimHank <orthopedicsalon5@gmail.com>
2022-05-10 21:28:37 +0300

    Added UserData, FibbonachiParams, Calculyator

--- src/app.py
+++ src/app.py
@@ -1,6 +1,23 @@
 from typing import Optional
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
+from pydantic import BaseModel, Field
+
+
+class UserData(BaseModel):
+    user_name: str
+    password: str
+
+
+class FibbonachiParams(BaseModel):
+    num: int
+    user: Optional[UserData]
+
+
+class Calculyator(BaseModel):
+    number: int = Field(..., gt=0, description="Number should be more than 0")
+    stepen: Optional[int] = Field(2, gt=0, description="Number should be more than 0")
+
 
 app = FastAPI()
 
@@ -15,12 +32,29 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("/fibbonachi/{n}")
-def fibbonachi(n: int):
-    #  """Рекурсия"""
+def calc(a, b):
+    return a**b
+
+
+@app.post("/calc")
+def calculate(payload: Calculyator):
+    # if payload.number > 0 and payload.stepen > 0:
+    res = calc(payload.number, payload.stepen)
+    return {"result": res}
+    # else:
+    #     raise HTTPException(status_code=400, detail="Number must be > 0")
+
+
+def fibbonachi_calculate(n):
     if n in {0, 1}:
         return n
-    result = fibbonachi(n - 1) + fibbonachi(n - 2)
+    result = fibbonachi_calculate(n - 1) + fibbonachi_calculate(n - 2)
     return result
 
+
+@app.post("/fibbonachi")
+def fibbonachi(n: FibbonachiParams):
+    #  """Рекурсия"""
+    result = fibbonachi_calculate(n.num)
+    return result
 
